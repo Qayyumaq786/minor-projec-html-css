ffa8e0ef608d0419c095f9626df8ab98e903835b
Qayyumaq786 <qayyumaq786@gmail.com>
2022-12-31 01:05:51 +0530

    responsive

--- script.js
+++ script.js
@@ -27,15 +27,15 @@ arrows.forEach((arrow, i) => {
 
 // toggle
 
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(
-  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
-);
+// const ball = document.querySelector(".toggle-ball");
+// const items = document.querySelectorAll(
+//   ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+// );
 
-ball.addEventListener("click", () => {
-  items.forEach((item) => {
-    item.classList.toggle("active");
-  });
-  ball.classList.toggle("active");
-});
+// ball.addEventListener("click", () => {
+//   items.forEach((item) => {
+//     item.classList.toggle("active");
+//   });
+//   ball.classList.toggle("active");
+// });
 
